4f42902db2bbd71562adb1718420b0ef64fb6656
devr <thememe3@gmail.com>
2019-07-05 22:59:30 -0500

    Cambios Login

--- src/index.js
+++ src/index.js
@@ -2,27 +2,36 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-
+const session = require('express-session');
 
 
 //settings 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 
 
 const alumnosRoutes= require('./routes/alumnos');
 const profesoresRoutes = require('./routes/profesores');
 const fechasRoutes = require('./routes/fechas');
+const loginRoutes = require('./routes/login')
 //middlewares
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-//app.use(express);
+app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 //routes
 app.use('/alumnos',alumnosRoutes);
 app.use('/profesores',profesoresRoutes);
 app.use('/fechas',fechasRoutes);
+app.use('/login',loginRoutes);
 
 //start server
 app.listen( app.get('port') , ()=>{
---
+++ src/routes/login.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+const mysqlConnection = require('../database');
+var query;
+router.route('/')
+.post((req, res) =>{
+    const {nombreUsuario, password} = req.body;	
+        if(nombreUsuario && password){
+            query = `SELECT * FROM Usuario WHERE nombreUsuario = ? AND password = ?;`
+            mysqlConnection.query(query, [nombreUsuario, password], (err, rows, fields) =>{
+                if(!err){
+                    if (rows.length > 0) {
+                        req.session.loggedin = true;
+                        req.session.nombreUsuario = nombreUsuario;
+                        res.send('verga si funciona');
+                    } else {
+                        res.send('Incorrect Username and/or Password!');
+                    }			    
+                    res.end();
+                }else{
+                    console.log(err);
+                }
+            })    
+        }else{
+            res.end();
+        }
+        
+    
+    //    res.send('Please enter Username and Password!');
+	//	res.end();
+    
+        
+
+})
+
+
+module.exports = router;
+
+
